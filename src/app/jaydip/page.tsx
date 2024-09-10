464d85b1f897530ad1e7c32f733f437ab11046bf
jaydipjadhav <jaydipdjadhav>
2024-09-10 12:04:46 +0530

    create user model

---
+++ src/app/jaydip/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Jaydip Dhananjay jadhav</div>
+  )
+}
+
+export default page
---
+++ src/model/user.model.ts
@@ -0,0 +1,89 @@
+import mongoose , {Schema , Document} from "mongoose";
+
+
+export interface Massageinterface extends Document{
+       content : string;
+       createdAt : Date;
+};
+
+
+const massageSchema : Schema<Massageinterface> = new Schema({
+
+     content : {
+        type : String,
+        required : true
+     },
+     createdAt : {
+        type : Date,
+        required : true,
+        default : Date.now
+     }
+})
+
+
+//user interface // datetype
+
+
+export interface Userinterface extends Document{
+    username : string;
+    email : string;
+    password : string;
+    verifycode : string;
+    isverifyfied : boolean;
+    verifycodeExpiry : Date;
+    isAcceptingMassage : boolean;
+    massages : Massageinterface[]
+}
+
+
+const userSchema : Schema<Userinterface> = new Schema({
+    username :{
+        type : String,
+        required: [true, " username is requried...."],
+        trim : true,
+        unique : true
+    },
+    email :{
+        type : String,
+        required: [true, " email is requried...."],
+        trim : true,
+        unique : true
+    },
+
+    password :{
+        type : String,
+        required: [true, " password is requried...."],
+    },
+    verifycode : {
+        type : String,
+        required: [true, " verifycode is requried...."],
+    },
+
+    isverifyfied : {
+        type : Boolean,
+        default : false
+
+    },
+    verifycodeExpiry : {
+        type : Date , 
+        required : true
+    },
+
+    isAcceptingMassage : {
+        type : Boolean,
+        defult : true
+    },
+    massages : [massageSchema]
+
+})
+
+
+
+// export model handling two cases :
+//1 .moddel in already create
+//2. new model create
+// add type script to defind type of model  //datetype
+
+const userModel = (mongoose.models.user as mongoose.Model<Userinterface>) || (mongoose.model<Userinterface>("user" , userSchema));
+
+export default userModel;
