3f2f71e5028d08b06ac39623a6476b43091c2ba2
jaydipjadhav <jaydipdjadhav>
2024-09-19 23:45:02 +0530

    get-messages

--- src/app/api/user/get-messages/route.ts
+++ src/app/api/user/get-messages/route.ts
@@ -6,6 +6,7 @@ import { authOptions } from "../../auth/[...nextauth]/option";
 import mongoose from "mongoose";
 
 
+
 export async function POST(request : Request){
 
     await dbconnect();
@@ -14,6 +15,9 @@ export async function POST(request : Request){
     const session = await getServerSession(authOptions);
       //user access in session
       const user :User = session?.user  as User;
+
+     console.log("user  : " , user);
+
 
       if(!session || !session.user){
         return Response.json({
@@ -28,10 +32,57 @@ export async function POST(request : Request){
       //id convert into mongodb obj id
       const userId = new mongoose.Types.ObjectId(user._id);
 
+      console.log(userId);
+
+
       try {
         
+      const user = await userModel.aggregate([
+          
+            { $match :  { _id : userId}} ,
+            { $unwind : "$messages"},
+            { $sort : {'messages.createdAt' : -1}},
+            {$group : {_id : "$_id" , messages : {
+              $push : "$messages"
+            }}}
+      
+        ])
+
+
+
+
+        if(!user || user.length === 0 ){
+
+          return Response.json({
+            success : false,
+            massage  : "user is not found.........",
+           
+          
+        } , {
+            status : 500
+        })
+
+        }
+
+        return Response.json({
+          success : true,
+          massage  : user[0].messages,
+         
+        
+      } , {
+          status : 200
+      })
         
       } catch (error) {
+
+        return Response.json({
+          success : false,
+          massage  : "user get message fetching error......",
+          error ,
+        
+      } , {
+          status : 500
+      })
         
       }
 
